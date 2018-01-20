bccee95da42d5414c0b53171fc0e164fe9075d92
Fenn <fennig1997@gmail.com>
2018-01-20 23:42:51 +0530

    Added login and register template

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
 
-
+import { AuthGuard } from './Guards/auth.guard';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './Components/nav-bar/nav-bar.component';
 import { DashboardComponent } from './Components/dashboard/dashboard.component';
 import { LoginComponent } from './Components/login/login.component';
 import { RegisterComponent } from './Components/register/register.component';
 import { PeopleComponent } from './Components/people/people.component';
+import { HomeComponent } from './Components/home/home.component';
+import { ProfileComponent } from './Components/profile/profile.component';
 
+const appRoutes: Routes =  [
+  {path: '', component: HomeComponent, pathMatch: 'full'},
+  {path: 'register', component: RegisterComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard]},
+  {path: 'profile', component: ProfileComponent, canActivate: [AuthGuard]}
+];
 
 @NgModule({
   declarations: [
@@ -17,10 +28,14 @@ import { PeopleComponent } from './Components/people/people.component';
     DashboardComponent,
     LoginComponent,
     RegisterComponent,
-    PeopleComponent
+    PeopleComponent,
+    HomeComponent,
+    ProfileComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
