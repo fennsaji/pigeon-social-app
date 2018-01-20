a7c4c9ba3782dc8ef29bf00b321a43820e7913c3
Fenn <fennig1997@gmail.com>
2018-01-20 16:59:23 +0530

    Initialise Front-end

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -3,11 +3,21 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './Components/nav-bar/nav-bar.component';
+import { DashboardComponent } from './Components/dashboard/dashboard.component';
+import { LoginComponent } from './Components/login/login.component';
+import { RegisterComponent } from './Components/register/register.component';
+import { PeopleComponent } from './Components/people/people.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    DashboardComponent,
+    LoginComponent,
+    RegisterComponent,
+    PeopleComponent
   ],
   imports: [
     BrowserModule
